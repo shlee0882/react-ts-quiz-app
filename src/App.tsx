b4399b629f2542a8775023d85e9a19efdd15b45b
SangHyun Lee <shlee0882@gmail.com>
2023-07-24 16:25:48 +0900

    구글 번역 테스트 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,53 @@
-import React, { useEffect, useState } from 'react';
+import React, { ChangeEvent, useLayoutEffect, useState } from 'react';
+import './App.css'; 
 
 function App() {
+  const [sourceText, setSourceText] = useState('');
   const [translatedText, setTranslatedText] = useState('');
+  const { REACT_APP_GOOGLE_TRANSLATE_API_KEY } = process.env;
+  const apiKey: string | undefined = REACT_APP_GOOGLE_TRANSLATE_API_KEY;
+  if (!apiKey) {
+    throw new Error('Missing Google API Key');
+  }
+  
+  useLayoutEffect(() => {
+    translate(sourceText, apiKey)
+      .then(response => {
+        console.log(response.data);
+        setTranslatedText(response?.data?.translations[0]?.translatedText);
+      })
+      .catch(reason => console.log(reason.message));
+  }, [sourceText, apiKey]);
 
-  useEffect(() => {
-    fetch('https://translation.googleapis.com/language/translate/v2', {
+
+  async function translate(strSourceText: string, strApiKey: string) {
+    var url = `https://translation.googleapis.com/language/translate/v2?key=${strApiKey}`
+    let response = await fetch(url, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        q: 'Hello world',
-        target: 'ko',
-        key: '',
-      }),
-    })
-    .then(response => response.json())
-    .then(data => {
-      if (data.error) {
-        console.error('Error:', data.error.message);
-      } else {
-        setTranslatedText(data.data.translations[0].translatedText);
-      }
-    })
-    .catch(error => console.error('Error:', error));
-  }, []);
+      headers: { 
+        'Content-Type': 'application/json',
+       },
+       body: JSON.stringify({
+         target: 'ko',
+         format: "text",
+         q: strSourceText
+       }),
+    }); 
+    let data = await response.json()
+    return data;
+  }
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSourceText(event.target.value);
+  }
 
   return (
     <div className="App">
-      <h1>Translation Test</h1>
-      <p>{translatedText}</p>
+      <h1>자동번역 테스트</h1>
+      <div className="input-container">
+        <input type="text" value={sourceText} onChange={handleInputChange} />
+      </div>
+      <p className="translated-text">{translatedText}</p>
     </div>
   );
 }
